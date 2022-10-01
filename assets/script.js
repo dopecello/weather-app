846aab41c7ad12792a861542817f9a8b07aeb463
dopecello <williamlocke.cello@gmail.com>
2022-10-01 15:34:37 -0400

    created fetchLocation method to grab lon and lat from user input

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,24 @@
 const app = {
+  init: () => {
+    document.getElementById("search-weather").addEventListener("click", app.fetchLocation);
+  },
   fetchLocation: () => {
-    let cityName = document.getElementById("city-input").value
-    let stateCode = 
-    let countryCode = 
-    let key = 
-  }
-}
+    let cityName = document.getElementById("city-input").value;
+    let stateCode = document.getElementById("state-input").value;
+    let countryCode = document.getElementById("country-input").value;
+    let key = "cc1eec67d9c183eb5563d87e72e89138";
+    let limit = 1;
+    let url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&limit=${limit}&appid=${key}`;
+    fetch(url)
+      .then((resp) => {
+        if (!resp.ok) throw new Error(resp.statusText);
+        return resp.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch(console.err);
+  },
+};
 
+app.init();
