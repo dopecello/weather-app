ab7074781950e2efa304de435a5175dcb5064845
dopecello <williamlocke.cello@gmail.com>
2022-10-02 10:15:40 -0400

    successfully retrieving data from the API

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 const app = {
   init: () => {
-    document.getElementById("search-weather").addEventListener("click", app.fetchLocation);
+    document
+      .getElementById("search-weather")
+      .addEventListener("click", app.fetchLocation);
   },
   fetchLocation: () => {
     let cityName = document.getElementById("city-input").value;
@@ -9,6 +11,24 @@ const app = {
     let key = "cc1eec67d9c183eb5563d87e72e89138";
     let limit = 1;
     let url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&limit=${limit}&appid=${key}`;
+    fetch(url)
+      .then((resp) => {
+        if (!resp.ok) throw new Error(resp.statusText);
+        return resp.json();
+      })
+      .then((data) => {
+        let usableData = {};
+        usableData.lat = data[0].lat;
+        usableData.lon = data[0].lon;
+        app.fetchWeather(usableData);
+      })
+      .catch(console.err);
+  },
+  fetchWeather: (weather) => {
+    let lat = weather.lat;
+    let lon = weather.lon;
+    let key = "cc1eec67d9c183eb5563d87e72e89138";
+    let url = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}&units=imperial&lang=en`;
     fetch(url)
       .then((resp) => {
         if (!resp.ok) throw new Error(resp.statusText);
